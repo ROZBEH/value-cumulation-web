f40428d98ddc469dc44c4c8a51a762537247ba1a
rozbeh <rouzbeh.asghari@gmail.com>
2022-07-24 18:16:28 -0700

    few more css for items inside main

--- web/src/pages/HomePage/HomePage.js
+++ web/src/pages/HomePage/HomePage.js
@@ -58,17 +58,17 @@ const HomePage = () => {
 
   return (
     <>
-      <div className="grid grid-rows-3 grid-cols-2 gap-4">
-        <div className="col-span-1">
+      <div className="grid grid-rows-3 grid-cols-2">
+        <div className="col-span-1 flex grid items-center mx-5">
           <Mainsubmission />
         </div>
-        <div className="row-span-3 content-center">
+        <div className="row-span-3">
           <Content />
         </div>
-        <div className="row-span-1 col-span-1">
+        <div className="row-span-1 col-span-1 flex items-center mx-5">
           <UserAddedMetric />
         </div>
-        <div className="row-span-1 col-span-1">
+        <div className="row-span-1 col-span-1 mx-3">
           <Mapping />
         </div>
       </div>
