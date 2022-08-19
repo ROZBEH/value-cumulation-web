4ad1d08bc4a7d98e293d223abdf0a0f10c370ac4
rozbeh <rouzbeh.asghari@gmail.com>
2022-08-18 19:37:31 -0700

    sec report support on the api side

--- api/src/graphql/fundamentalanalysis.sdl.js
+++ api/src/graphql/fundamentalanalysis.sdl.js
@@ -2,11 +2,21 @@ export const schema = gql`
   # the following will query a specific company and get it's financials data
   # It will return company name as well as the financials data and the data
   # that accompanies it
+  type secReport {
+    symbol: String!
+    fillingDate: String!
+    acceptedDate: String!
+    cik: String!
+    type: String!
+    link: String!
+    finalLink: String!
+  }
   type FetchMetrics {
     companyName: String!
     metricNames: [String!]!
     fullMetricNames: [String!]!
     metricValues: [[Float]!]!
+    secReports: [secReport!]!
     years: [[String!]!]!
   }
 
