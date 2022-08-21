31d727938edd0ba1a5bc53a3c20ac2d9254a9803
rozbeh <rouzbeh.asghari@gmail.com>
2022-08-20 20:36:35 -0700

    add three search boxes

--- web/src/components/SECLinks/SECLinks.js
+++ web/src/components/SECLinks/SECLinks.js
@@ -9,24 +9,48 @@ import {
 export const SECLinks = () => {
   const calledCompanies = useRecoilValue(calledCompaniesAtom)
   const secReport = useRecoilValue(secReportsAtom)
-  const [userSelect, setUserSelect] = useState({})
-  const myChangeFunc = async (_event, values, reason, _details) => {
+  const [reportNameArrays, setReportNameArrays] = useState([])
+  const [allReportArrays, setAllReportArrays] = useState([])
+  const [pickedReportArrays, setPickedReportArrays] = useState([])
+
+  const onSelectCompany = async (_event, values, reason, _details) => {
     if (reason === 'selectOption') {
-      setUserSelect(values)
+      const selectedCompanyReport = secReport[values.name]
+      setAllReportArrays(selectedCompanyReport)
+      setReportNameArrays(Object.keys(selectedCompanyReport))
     } else if (reason === 'clear') {
-      setUserSelect({})
+      setAllReportArrays([])
+      setReportNameArrays([])
+      setPickedReportArrays([])
+    }
+  }
+
+  const onSelectReport = async (_event, values, reason, _details) => {
+    if (reason === 'selectOption') {
+      setPickedReportArrays(allReportArrays[values])
+    } else if (reason === 'clear') {
+      setPickedReportArrays([])
+    }
+  }
+
+  const onSelectDate = async (_event, values, reason, _details) => {
+    if (reason === 'selectOption') {
+      console.log('picked')
+    } else if (reason === 'clear') {
+      console.log('cleared')
     }
   }
   return (
     <div>
       {calledCompanies.length > 0 && (
         <div>
+          {/* Pick Company */}
           <Autocomplete
             onBlur={() => {
               setTimeout(() => {}, 100)
             }}
             onChange={(event, values, reason, details) =>
-              myChangeFunc(event, values, reason, details)
+              onSelectCompany(event, values, reason, details)
             }
             isOptionEqualToValue={(option, value) => option.id === value.id}
             options={calledCompanies}
@@ -39,6 +63,52 @@ export const SECLinks = () => {
                   variant="standard"
                   fullWidth
                   placeholder="Enter Company Name"
+                />
+              )
+            }}
+          />
+          {/* Pick Report */}
+          <Autocomplete
+            onBlur={() => {
+              setTimeout(() => {}, 100)
+            }}
+            onChange={(event, values, reason, details) =>
+              onSelectReport(event, values, reason, details)
+            }
+            isOptionEqualToValue={(option, value) => option.id === value.id}
+            options={reportNameArrays}
+            getOptionLabel={(option) => option}
+            renderInput={(params) => {
+              return (
+                <TextField
+                  className="text-field-searchbar"
+                  {...params}
+                  variant="standard"
+                  fullWidth
+                  placeholder="Enter Report Name"
+                />
+              )
+            }}
+          />
+          {/* Pick Date */}
+          <Autocomplete
+            onBlur={() => {
+              setTimeout(() => {}, 100)
+            }}
+            onChange={(event, values, reason, details) =>
+              onSelectDate(event, values, reason, details)
+            }
+            isOptionEqualToValue={(option, value) => option.id === value.id}
+            options={pickedReportArrays}
+            getOptionLabel={(option) => option.fillingDate}
+            renderInput={(params) => {
+              return (
+                <TextField
+                  className="text-field-searchbar"
+                  {...params}
+                  variant="standard"
+                  fullWidth
+                  placeholder="Pick a Report Date"
                 />
               )
             }}
