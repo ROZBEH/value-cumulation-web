f89df74466fd1d4ab0bab34ba72bc97e934dd3c8
rozbeh <rouzbeh.asghari@gmail.com>
2022-08-20 20:50:24 -0700

    add button for the report link

--- web/src/components/SECLinks/SECLinks.js
+++ web/src/components/SECLinks/SECLinks.js
@@ -1,7 +1,7 @@
 import { useState } from 'react'
 import { useRecoilValue } from 'recoil'
 import Autocomplete from '@mui/material/Autocomplete'
-import { Tooltip, TextField } from '@material-ui/core'
+import { Tooltip, TextField, Button } from '@material-ui/core'
 import {
   secReports as secReportsAtom,
   calledCompanies as calledCompaniesAtom,
@@ -12,6 +12,7 @@ export const SECLinks = () => {
   const [reportNameArrays, setReportNameArrays] = useState([])
   const [allReportArrays, setAllReportArrays] = useState([])
   const [pickedReportArrays, setPickedReportArrays] = useState([])
+  const [reportLink, setReportLink] = useState('')
 
   const onSelectCompany = async (_event, values, reason, _details) => {
     if (reason === 'selectOption') {
@@ -30,91 +31,110 @@ export const SECLinks = () => {
       setPickedReportArrays(allReportArrays[values])
     } else if (reason === 'clear') {
       setPickedReportArrays([])
+      setReportLink('')
     }
   }
 
   const onSelectDate = async (_event, values, reason, _details) => {
     if (reason === 'selectOption') {
-      console.log('picked')
+      setReportLink(values.link)
     } else if (reason === 'clear') {
-      console.log('cleared')
+      setReportLink('')
     }
   }
   return (
-    <div>
-      {calledCompanies.length > 0 && (
+    <>
+      <div>
         <div>
-          {/* Pick Company */}
-          <Autocomplete
-            onBlur={() => {
-              setTimeout(() => {}, 100)
-            }}
-            onChange={(event, values, reason, details) =>
-              onSelectCompany(event, values, reason, details)
-            }
-            isOptionEqualToValue={(option, value) => option.id === value.id}
-            options={calledCompanies}
-            getOptionLabel={(option) => `${option.name} (${option.symbol})`}
-            renderInput={(params) => {
-              return (
-                <TextField
-                  className="text-field-searchbar"
-                  {...params}
-                  variant="standard"
-                  fullWidth
-                  placeholder="Enter Company Name"
-                />
-              )
-            }}
-          />
-          {/* Pick Report */}
-          <Autocomplete
-            onBlur={() => {
-              setTimeout(() => {}, 100)
-            }}
-            onChange={(event, values, reason, details) =>
-              onSelectReport(event, values, reason, details)
-            }
-            isOptionEqualToValue={(option, value) => option.id === value.id}
-            options={reportNameArrays}
-            getOptionLabel={(option) => option}
-            renderInput={(params) => {
-              return (
-                <TextField
-                  className="text-field-searchbar"
-                  {...params}
-                  variant="standard"
-                  fullWidth
-                  placeholder="Enter Report Name"
-                />
-              )
-            }}
-          />
-          {/* Pick Date */}
-          <Autocomplete
-            onBlur={() => {
-              setTimeout(() => {}, 100)
-            }}
-            onChange={(event, values, reason, details) =>
-              onSelectDate(event, values, reason, details)
-            }
-            isOptionEqualToValue={(option, value) => option.id === value.id}
-            options={pickedReportArrays}
-            getOptionLabel={(option) => option.fillingDate}
-            renderInput={(params) => {
-              return (
-                <TextField
-                  className="text-field-searchbar"
-                  {...params}
-                  variant="standard"
-                  fullWidth
-                  placeholder="Pick a Report Date"
-                />
-              )
-            }}
-          />
+          {calledCompanies.length > 0 && (
+            <div>
+              {/* Pick Company */}
+              <Autocomplete
+                onBlur={() => {
+                  setTimeout(() => {}, 100)
+                }}
+                onChange={(event, values, reason, details) =>
+                  onSelectCompany(event, values, reason, details)
+                }
+                isOptionEqualToValue={(option, value) => option.id === value.id}
+                options={calledCompanies}
+                getOptionLabel={(option) => `${option.name} (${option.symbol})`}
+                renderInput={(params) => {
+                  return (
+                    <TextField
+                      className="text-field-searchbar"
+                      {...params}
+                      variant="standard"
+                      fullWidth
+                      placeholder="Enter Company Name"
+                    />
+                  )
+                }}
+              />
+              {/* Pick Report */}
+              <Autocomplete
+                onBlur={() => {
+                  setTimeout(() => {}, 100)
+                }}
+                onChange={(event, values, reason, details) =>
+                  onSelectReport(event, values, reason, details)
+                }
+                isOptionEqualToValue={(option, value) => option.id === value.id}
+                options={reportNameArrays}
+                getOptionLabel={(option) => option}
+                renderInput={(params) => {
+                  return (
+                    <TextField
+                      className="text-field-searchbar"
+                      {...params}
+                      variant="standard"
+                      fullWidth
+                      placeholder="Enter Report Name"
+                    />
+                  )
+                }}
+              />
+              {/* Pick Date */}
+              <Autocomplete
+                onBlur={() => {
+                  setTimeout(() => {}, 100)
+                }}
+                onChange={(event, values, reason, details) =>
+                  onSelectDate(event, values, reason, details)
+                }
+                isOptionEqualToValue={(option, value) => option.id === value.id}
+                options={pickedReportArrays}
+                getOptionLabel={(option) => option.fillingDate}
+                renderInput={(params) => {
+                  return (
+                    <TextField
+                      className="text-field-searchbar"
+                      {...params}
+                      variant="standard"
+                      fullWidth
+                      placeholder="Pick a Report Date"
+                    />
+                  )
+                }}
+              />
+            </div>
+          )}
         </div>
-      )}
-    </div>
+
+        {reportLink && (
+          <div className="clear-both">
+            <Tooltip title="Report Link">
+              <button
+                className="rounded-lg w-20 h-8 bg-lightsky-blue border border-gray-300 text-white text-xs cursor-pointer ml-1"
+                name="comparisonMode"
+                onClick={() => window.open(reportLink)}
+              >
+                Report Link
+              </button>
+            </Tooltip>
+          </div>
+        )}
+      </div>
+    </>
   )
 }
