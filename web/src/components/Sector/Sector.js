a496e3419d97f2a024db54a9e5ed4a8759a03706
rozbeh <rouzbeh.asghari@gmail.com>
2022-08-29 21:16:29 -0700

    add sector component

---
+++ web/src/components/Sector/Sector.js
@@ -0,0 +1,8 @@
+export const Sector = () => {
+  return (
+    <div>
+      <h2>{'Sector'}</h2>
+      <p>{'Find me in ./web/src/components/Sector/Sector.js'}</p>
+    </div>
+  )
+}
---
+++ web/src/components/Sector/Sector.stories.js
@@ -0,0 +1,7 @@
+import Sector from './Sector'
+
+export const generated = () => {
+  return <Sector />
+}
+
+export default { title: 'Components/Sector' }
