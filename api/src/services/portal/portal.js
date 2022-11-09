c26f4933fad86002b650f5b45d962ddd177258f5
rozbeh <rouzbeh.asghari@gmail.com>
2022-11-09 11:55:58 -0800

    add portal service

---
+++ api/src/services/portal/portal.js
@@ -0,0 +1,31 @@
+import { stripe } from 'src/lib/stripe'
+/**
+ * @param {{ userId: int }}
+ */
+export const portal = async ({ userId }) => {
+  // eslint-disable-next-line camelcase
+  // Create Customer Portal session and send temp url to web side
+  return await stripe.billingPortal.sessions.create({
+    customer: userId,
+    return_url: `${
+      context.request?.headers?.referer ?? 'http://localhost:8910'
+    }`,
+  })
+}
+
+export const subscriptionHistory = async ({ userId }) => {
+  // const userInfo = await db.user.findUnique({
+  //   where: { id },
+  //   include: { favoriteMetrics: { include: { favoriteMetric: true } } },
+  // })
+  // Take the user information and see whether the user has subscribed to any plans
+  const customerSubscription = await stripe.subscriptions.list({
+    customer: userId,
+  })
+  // If the user has subscribed to any plans, return the plan information
+  if (customerSubscription.data.length > 0) {
+    return { hadSubscription: true }
+  } else {
+    return { hadSubscription: false }
+  }
+}
