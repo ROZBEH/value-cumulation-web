85289ba5ffd2a7459b30a8ae012708654e0efe16
rozbeh <rouzbeh.asghari@gmail.com>
2022-11-09 11:56:11 -0800

    add portal sdl

---
+++ api/src/graphql/portal.sdl.js
@@ -0,0 +1,19 @@
+export const schema = gql`
+  type Session {
+    id: String!
+    url: String!
+  }
+
+  type SubHistory {
+    hadSubscription: Boolean!
+  }
+
+  type Query {
+    subscriptionHistory(userId: ID!): SubHistory! @skipAuth
+  }
+
+  type Mutation {
+    # In GraphQL, we can't reuse types as mutation inputs
+    portal(userId: ID!): Session! @requireAuth
+  }
+`
