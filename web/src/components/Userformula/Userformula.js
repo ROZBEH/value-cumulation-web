5171c5bcfba018620f608bc08114188f2be41f03
rozbeh <rouzbeh.asghari@gmail.com>
2022-11-30 06:26:10 -0800

    update userformula component with lazyQuery

--- web/src/components/Userformula/Userformula.js
+++ web/src/components/Userformula/Userformula.js
@@ -1,23 +1,37 @@
+import { useLazyQuery } from '@apollo/client'
 import FormLabel from '@mui/material/FormLabel'
 
 import { AVAILABLE_METRICS } from 'src/commons/constants'
+import { FILTERED_COMPANIES } from 'src/commons/gql'
 import { Metricsearch } from 'src/components/Metricsearch/Metricsearch'
 
 export const Userformula = () => {
+  const [filteredCompanyList] = useLazyQuery(FILTERED_COMPANIES, {
+    onCompleted: (data) => {
+      console.log('data: ', data)
+    },
+  })
+
   const onSubmit = (event) => {
+    var input = []
     for (let i = 0; i < event.target.length; i++) {
       if (event.target[i].tagName === 'INPUT') {
         let currentMetric = AVAILABLE_METRICS.find(
           (item) => item.title === event.target[i].value
         )
         if (currentMetric) {
-          console.log(currentMetric.value)
+          input.push({
+            name: currentMetric.value,
+          })
         } else {
-          console.log(event.target[i].value)
+          input[input.length - 1].value = parseFloat(event.target[i].value)
         }
         event.preventDefault()
       }
     }
+    filteredCompanyList({
+      variables: { input: input },
+    })
   }
 
   return (
@@ -28,7 +42,8 @@ export const Userformula = () => {
         className="flex flex-col w-3/12"
       >
         <Metricsearch name="test" />
-        <FormLabel className="mt-5"></FormLabel>
+        <Metricsearch name="test2" />
+        <FormLabel className="mt-5"> Submit the request</FormLabel>
         <button
           type="submit"
           className="rounded-lg w-20 h-8 bg-lightsky-blue border border-gray-300 text-white cursor-pointer ml-1 mt-4"
