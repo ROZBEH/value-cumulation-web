7ea74721e2052b10a650d79083d8c08d153190b7
rozbeh <rouzbeh.asghari@gmail.com>
2022-12-01 07:10:56 -0800

    add support for Bulk Endpoint

--- api/src/services/userformula/userformula.js
+++ api/src/services/userformula/userformula.js
@@ -7,13 +7,48 @@ You are strictly prohibited from distributing or using this repository unless ot
  */
 import { fetch } from 'cross-undici-fetch'
 
-import { companyslist } from 'src/services/searchbar/searchbar'
+function csvToJson(csv) {
+  // \n or \r\n depending on the EOL sequence
+  const lines = csv.split('\r\n').filter((line) => line.length != 0)
+  const delimeter = ','
+
+  const result = []
+
+  const headers = lines[0].split(delimeter)
+
+  for (const line of lines) {
+    const obj = {}
+    const row = line.split(delimeter)
+
+    for (let i = 0; i < headers.length; i++) {
+      const header = headers[i]
+      obj[header] = row[i]
+    }
+
+    result.push(obj)
+  }
+
+  // Prettify output
+  // return JSON.stringify(result, null, 2)
+  return result
+}
 
 export const getFilteredCompanies = async ({ inputMetrics }) => {
-  console.log('I am inside userformula service')
-  console.log('inputMetrics = ', inputMetrics)
-  const companyList = await companyslist()
-  console.log('companyList = ', companyList[0])
+  const request = await fetch(
+    `https://financialmodelingprep.com/api/v4/income-statement-bulk?year=2020&period=annual&apikey=${process.env.FINANCIAL_API_KEY}`,
+    {
+      json: true,
+    }
+  )
+    .then((res) => res.blob())
+    .then((blob) => blob.text())
+    // Convert the String(CSV format) to JSON
+    .then((text) => csvToJson(text))
+
+  console.log('request[0] = ', request[0])
+  console.log('request[1] = ', request[1])
+  console.log('request[2] = ', request[2])
+  console.log('request[-1] = ', request[request.length - 1])
 
   return { names: ['Test 1', 'Test 2'] }
 }
