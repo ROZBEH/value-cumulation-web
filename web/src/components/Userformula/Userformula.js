ad79860aebdbf1196dce14748bee6a70c6cf3720
rozbeh <rouzbeh.asghari@gmail.com>
2022-12-02 12:04:28 -0800

    add legal stuff

--- web/src/components/Userformula/Userformula.js
+++ web/src/components/Userformula/Userformula.js
@@ -1,3 +1,10 @@
+/**
+Value Cumulation
+Copyright (c) 2022 Value Cumulation
+
+Notice: All code and information in this repository is the property of Value Cumulation.
+You are strictly prohibited from distributing or using this repository unless otherwise stated.
+ */
 import { useLazyQuery } from '@apollo/client'
 import FormLabel from '@mui/material/FormLabel'
 
