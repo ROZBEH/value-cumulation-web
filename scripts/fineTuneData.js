c6eaba7616f108f6248ec5c42b42474d9751c8e5
rozbeh <rouzbeh.asghari@gmail.com>
2022-12-29 23:34:13 -0800

    scripts for testing gpt3

---
+++ scripts/fineTuneData.js
@@ -0,0 +1,16 @@
+/**
+Value Cumulation
+Copyright (c) 2022 Value Cumulation
+
+Notice: All code and information in this repository is the property of Value Cumulation.
+You are strictly prohibited from distributing or using this repository unless otherwise stated.
+ */
+
+export const fineTuneData = `Q: AAPL
+A: ["MSFT", "FB", "GOOG", "TSLA"]
+
+Q: COST
+A: ["WMT", "AMZN", "EBAY", "SHLDQ"]
+
+Q: BP
+A: ["XOM", "CVX", "MPC", "VLO"]`
