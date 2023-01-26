142750e0c62acb459bb879f2931c66c428a76c7b
rozbeh <rouzbeh.asghari@gmail.com>
2023-01-25 21:47:03 -0800

    password condition checking

--- web/src/pages/SignupPage/SignupPage.js
+++ web/src/pages/SignupPage/SignupPage.js
@@ -6,10 +6,12 @@ Notice: All code and information in this repository is the property of Value Cum
 You are strictly prohibited from distributing or using this repository unless otherwise stated.
  */
 
-import { useRef } from 'react'
-import { useEffect } from 'react'
+import { useRef, useState, useEffect } from 'react'
 
 import { useForm as useSpreeForm } from '@formspree/react'
+import CloseIcon from '@mui/icons-material/Close'
+import DoneIcon from '@mui/icons-material/Done'
+import { useForm } from 'react-hook-form'
 
 import { useAuth } from '@redwoodjs/auth'
 import {
@@ -39,6 +41,47 @@ const SignupPage = () => {
   useEffect(() => {
     usernameRef.current.focus()
   }, [])
+  const password = useRef()
+  const formMethods = useForm()
+  password.current = formMethods.watch('password', '')
+  const [errors, setErrors] = useState({})
+
+  const checkErrors = (password) => {
+    const length = password.length >= 8
+    const uppercase = /[A-Z]/.test(password)
+    const lowercase = /[a-z]/.test(password)
+    const number = /[0-9]/.test(password)
+    const special = /[!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?]/.test(password)
+    const whitespace = !/\s/.test(password)
+
+    setErrors({
+      length: length,
+      uppercase: uppercase,
+      lowercase: lowercase,
+      number: number,
+      special: special,
+      whitespace: whitespace,
+    })
+  }
+  const errMsg = (error) => {
+    if (error === 'length') {
+      return 'Password must be at least 8 characters'
+    } else if (error === 'uppercase') {
+      return 'Password must contain at least one uppercase letter'
+    } else if (error === 'lowercase') {
+      return 'Password must contain at least one lowercase letter'
+    } else if (error === 'number') {
+      return 'Password must contain at least one number'
+    } else if (error === 'special') {
+      return 'Password must contain at least one special character'
+    } else if (error === 'whitespace') {
+      return 'Password must not contain any whitespace'
+    }
+  }
+  // This is a hack to clear the errors when the user deletes the password
+  if ((password.current === null) & (Object.keys(errors).length > 0)) {
+    setErrors({})
+  }
 
   const onSubmit = async (data) => {
     spreeSubmit({ username: data.username, name: data.name })
@@ -72,13 +115,17 @@ const SignupPage = () => {
 
             <div className="rw-segment-main">
               <div className="rw-form-wrapper">
-                <Form onSubmit={onSubmit} className="rw-form-wrapper">
+                <Form
+                  formMethods={formMethods}
+                  onSubmit={onSubmit}
+                  className="rw-form-wrapper"
+                >
                   <Label
                     name="username"
                     className="rw-label"
                     errorClassName="rw-label rw-label-error"
                   >
-                    Username
+                    Email
                   </Label>
                   <TextField
                     name="username"
@@ -90,10 +137,13 @@ const SignupPage = () => {
                         value: true,
                         message: 'Username is required',
                       },
+                      pattern: {
+                        value: /[^@]+@[^.]+\..{2,}/,
+                        message: 'Please enter a valid email address',
+                      },
                     }}
                   />
-
-                  <FieldError name="name" className="rw-field-error" />
+                  <FieldError name="username" className="rw-field-error" />
 
                   <Label
                     name="name"
@@ -127,22 +177,10 @@ const SignupPage = () => {
                     name="password"
                     className="rw-input"
                     errorClassName="rw-input rw-input-error"
-                    autoComplete="current-password"
+                    autoComplete="new-password"
                     validation={{
-                      validate: (value) => {
-                        if (value.length < 8) {
-                          return 'Password must be at least 8 characters'
-                        } else if (!value.match(/[A-Z]/)) {
-                          return 'Password must contain at least one uppercase letter'
-                        } else if (!value.match(/[a-z]/)) {
-                          return 'Password must contain at least one lowercase letter'
-                        } else if (!value.match(/[0-9]/)) {
-                          return 'Password must contain at least one number'
-                        } else if (!value.match(/[!@#$%^&*()]/)) {
-                          return 'Password must contain at least one special character'
-                        } else {
-                          return true
-                        }
+                      validate: (password) => {
+                        checkErrors(password)
                       },
                       required: {
                         value: true,
@@ -150,8 +188,52 @@ const SignupPage = () => {
                       },
                     }}
                   />
+                  {/* <FieldError name="password" className="rw-field-verified" /> */}
+                  {/* <FieldError name="password" className="rw-field-error" /> */}
+                  {Object.keys(errors).map((error, index) => {
+                    let color = errors[error] ? 'green' : 'red'
+                    return (
+                      <div key={index}>
+                        <div
+                          className={
+                            color == 'red'
+                              ? 'rw-field-error'
+                              : 'rw-field-verified'
+                          }
+                        >
+                          {color === 'green' ? (
+                            <DoneIcon style={{ color: color }} />
+                          ) : (
+                            <CloseIcon style={{ color: color }} />
+                          )}
+                          {errMsg(error)}
+                        </div>
+                      </div>
+                    )
+                  })}
 
-                  <FieldError name="password" className="rw-field-error" />
+                  <Label
+                    className="rw-label"
+                    errorClassName="rw-label rw-label-error"
+                    name="password_confirm"
+                  >
+                    Confirm Password:
+                  </Label>
+                  <PasswordField
+                    name="password_confirm"
+                    className="rw-input"
+                    errorClassName="rw-input rw-input-error"
+                    autoComplete="new-password"
+                    validation={{
+                      required: 'You must confirm your password',
+                      validate: (value) =>
+                        value === password.current || 'Passwords must match',
+                    }}
+                  />
+                  <FieldError
+                    name="password_confirm"
+                    className="rw-field-error"
+                  />
 
                   <div className="rw-button-group">
                     <Submit className="rw-button rw-button-blue">
