59e17fd2d2e47684aa3b594c4966e4dd62c80d09
rozbeh <rouzbeh.asghari@gmail.com>
2023-02-01 19:48:22 -0800

    secure is false

--- api/src/functions/emails/mailer.js
+++ api/src/functions/emails/mailer.js
@@ -9,6 +9,7 @@ export async function sendEmail({ to, subject, text, html }) {
   const transporter = nodemailer.createTransport({
     host: process.env.SMTP_HOST,
     port: process.env.SMTP_PORT,
+    secure: false,
     auth: {
       user: process.env.SMTP_USER,
       pass: process.env.SMTP_PASS,
