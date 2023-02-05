7197137c43fe7a428c8dedff5b78b86b4fba628c
rozbeh <rouzbeh.asghari@gmail.com>
2023-02-04 18:41:51 -0800

    change nodemailer call

--- api/src/lib/mailer.js
+++ api/src/lib/mailer.js
@@ -1,13 +1,24 @@
 import * as nodemailer from 'nodemailer'
 
 export async function sendEmail({ to, subject, text, html }) {
+  // const transporter = nodemailer.createTransport({
+  //   host: process.env.SMTP_HOST,
+  //   port: process.env.SMTP_PORT,
+  //   secure: false,
+  //   auth: {
+  //     user: process.env.SMTP_USER,
+  //     pass: process.env.SMTP_PASS,
+  //   },
+  // })
   const transporter = nodemailer.createTransport({
-    host: process.env.SMTP_HOST,
-    port: process.env.SMTP_PORT,
-    secure: false,
+    service: 'gmail',
     auth: {
+      type: 'OAuth2',
       user: process.env.SMTP_USER,
       pass: process.env.SMTP_PASS,
+      clientId: process.env.SMTP_CLIENT_ID,
+      clientSecret: process.env.SMTP_CLIENT_SECRET,
+      refreshToken: process.env.SMTP_REFRESH_TOKEN,
     },
   })
   console.log('before sending email')
