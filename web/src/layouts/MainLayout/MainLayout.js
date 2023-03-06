bb9c9a0c1360e2fb391832b53d95da16cd4a191c
rozbeh <rouzbeh.asghari@gmail.com>
2023-03-05 22:40:43 -0800

    fix login button so it's clickable anywhere

--- web/src/layouts/MainLayout/MainLayout.js
+++ web/src/layouts/MainLayout/MainLayout.js
@@ -46,7 +46,7 @@ const MainLayout = ({ children }) => {
               <button
                 onClick={logOut}
                 type="button"
-                className="text-gray-900 bg-white border border-gray-300 focus:outline-none hover:bg-gray-100 focus:ring-4 focus:ring-gray-200 font-medium rounded-lg text-sm px-3 py-2 mr-2 mb-2 dark:bg-gray-800 dark:text-white dark:border-gray-600 dark:hover:bg-gray-700 dark:hover:border-gray-600 dark:focus:ring-gray-700"
+                className="inline-block p-3 text-gray-900 bg-white border border-gray-300 focus:outline-none hover:bg-gray-100 focus:ring-4 focus:ring-gray-200 font-medium rounded-lg text-sm dark:bg-gray-800 dark:text-white dark:border-gray-600 dark:hover:bg-gray-700 dark:hover:border-gray-600 dark:focus:ring-gray-700"
               >
                 Logout
               </button>
@@ -55,9 +55,11 @@ const MainLayout = ({ children }) => {
             <div className="block mt-4 sm:inline-block sm:mt-0 text-base text-lime-700 mr-8">
               <button
                 type="button"
-                className="text-gray-900 bg-white border border-gray-300 focus:outline-none hover:bg-gray-100 focus:ring-4 focus:ring-gray-200 font-medium rounded-lg text-sm px-3 py-2 mr-2 mb-2 dark:bg-gray-800 dark:text-white dark:border-gray-600 dark:hover:bg-gray-700 dark:hover:border-gray-600 dark:focus:ring-gray-700"
+                className="text-gray-900 bg-white border border-gray-300 focus:outline-none hover:bg-gray-100 focus:ring-4 focus:ring-gray-200 font-medium rounded-lg text-sm dark:bg-gray-800 dark:text-white dark:border-gray-600 dark:hover:bg-gray-700 dark:hover:border-gray-600 dark:focus:ring-gray-700"
               >
-                <Link to={routes.login()}>Login</Link>
+                <Link className="block p-3" to={routes.login()}>
+                  Login
+                </Link>
               </button>
             </div>
           ) : null}
