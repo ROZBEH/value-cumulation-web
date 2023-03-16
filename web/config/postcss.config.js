ee773bf0436d5654725a38516df219dac1d6d0ad
rozbeh <rouzbeh.asghari@gmail.com>
2023-03-15 23:43:56 -0700

    whitelist react toastify

--- web/config/postcss.config.js
+++ web/config/postcss.config.js
@@ -5,6 +5,9 @@ const purgecss = require('@fullhuman/postcss-purgecss')({
     path.resolve(__dirname, '..', 'src', '**', '!(*.test).js'),
     path.resolve(__dirname, '..', 'src', '**', '*.html'),
   ],
+  options: {
+    whitelist: ['react-toastify/dist/ReactToastify.css'],
+  },
   // This extractor is used for tailwind classes.
   // Read more here: https://tailwindcss.com/docs/controlling-file-size/
   defaultExtractor: (content) => content.match(/[\w-/:]+(?<!:)/g) || [],
