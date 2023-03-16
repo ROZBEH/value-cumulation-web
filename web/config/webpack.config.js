7ed489b7b1770c158df5a493ab4ad3382fe9886c
rozbeh <rouzbeh.asghari@gmail.com>
2023-03-15 21:54:47 -0700

    fix css loader

--- web/config/webpack.config.js
+++ web/config/webpack.config.js
@@ -1,21 +1,27 @@
 const configDir = __dirname
 
 module.exports = (config) => {
+  // Modify the existing CSS rule to include postcss-loader
+  config.module.rules[0].oneOf[5].use.push({
+    loader: 'postcss-loader',
+    options: {
+      postcssOptions: {
+        config: `${configDir}/postcss.config.js`,
+      },
+    },
+  })
+
+  // Add new rule to handle ReactToastify.css and other CSS files
   config.module.rules[0].oneOf.unshift({
     test: /(ReactToastify\.css|\.css)$/,
     use: [
       'style-loader',
-      {
-        loader: 'css-loader',
-        options: {
-          importLoaders: 1,
-        },
-      },
+      { loader: 'css-loader', options: { importLoaders: 1 } },
       {
         loader: 'postcss-loader',
         options: {
-          config: {
-            path: configDir,
+          postcssOptions: {
+            config: `${configDir}/postcss.config.js`,
           },
         },
       },
