54bd21145ccb4ba25ab89ff6e96023335afc1708
rozbeh <rouzbeh.asghari@gmail.com>
2023-04-04 21:53:03 -0700

    add portal and checkout links

--- web/src/components/StripeCheckout/StripeCheckout.js
+++ web/src/components/StripeCheckout/StripeCheckout.js
@@ -1,29 +1,77 @@
-import { useStripe } from '@stripe/react-stripe-js'
+import { useEffect } from 'react'
+
+import { toast } from 'react-toastify'
 
 import { useAuth } from '@redwoodjs/auth'
 import { useMutation } from '@redwoodjs/web'
 
 import { CREATE_CHECKOUT_SESSION } from 'src/commons/gql'
-
 const StripeCheckout = () => {
   const { currentUser } = useAuth()
-  const stripe = useStripe()
-  const [createCheckoutSession] = useMutation(CREATE_CHECKOUT_SESSION, {
-    onCompleted: async ({ createCheckoutSession }) => {
-      const { sessionId } = createCheckoutSession
-      await stripe.redirectToCheckout({ sessionId })
-    },
-    onError: (error) => {
-      console.error('Error creating Stripe checkout session:', error)
-    },
-  })
 
-  const redirectToCheckout = () => {
+  const [createCheckoutSession, { error }] = useMutation(
+    CREATE_CHECKOUT_SESSION
+  )
+  useEffect(() => {
+    if (error) {
+      toast.error('Error creating Stripe checkout session:', error)
+    }
+  }, [error])
+  function timeout(delay) {
+    return new Promise((res) => setTimeout(res, delay))
+  }
+  const redirectToCheckout = async () => {
+    const checkoutButton = document.querySelector('#checkout-button')
+    checkoutButton.disabled = true
     const priceId = 'price_1LlWpoGCTzbTVORHcCZ8iHOx' // Replace with the desired price ID
-    createCheckoutSession({ variables: { priceId, userId: currentUser.id } })
+    try {
+      const { data } = await createCheckoutSession({
+        variables: { priceId, userId: currentUser.id },
+      })
+      const { sessionUrl } = data.createCheckoutSession
+
+      // Open Stripe Checkout in a new window
+      toast.success('Opening in a new window tab')
+      await timeout(2000) //for 2 seconds delay
+      const newWindow = window.open(sessionUrl, '_blank')
+
+      // Check if the new window was blocked by a popup blocker
+      if (
+        !newWindow ||
+        newWindow.closed ||
+        typeof newWindow.closed === 'undefined'
+      ) {
+        alert(
+          'A popup blocker may have prevented the Stripe Checkout from opening. Please disable the popup blocker and try again.'
+        )
+      }
+
+      // Listen for the new window to close
+      const checkWindowClosed = setInterval(() => {
+        if (newWindow.closed) {
+          clearInterval(checkWindowClosed)
+
+          // Re-enable the button
+          checkoutButton.disabled = false
+        }
+      }, 100)
+    } catch (error) {
+      toast.error('Error creating Stripe checkout session:', error)
+
+      // Re-enable the button
+      checkoutButton.disabled = false
+    }
   }
 
-  return <button onClick={redirectToCheckout}>Manage Subscription</button>
+  return (
+    <button
+      className="opacity-70 btn-primary rounded-lg bg-gray-600 py-3 px-2 font-semibold hover:bg-gray-400"
+      id="checkout-button"
+      onClick={redirectToCheckout}
+    >
+      Add Subscription
+    </button>
+  )
 }
 
 export default StripeCheckout
--- web/src/components/StripePortal/StripePortal.js
+++ web/src/components/StripePortal/StripePortal.js
@@ -1,19 +1,37 @@
+import { toast } from 'react-toastify'
+
 import { useAuth } from '@redwoodjs/auth'
 import { useMutation } from '@redwoodjs/web'
 
 import { CREATE_BILLING_PORTAL_SESSION } from 'src/commons/gql'
 
+function timeout(delay) {
+  return new Promise((res) => setTimeout(res, delay))
+}
 const StripePortal = () => {
   const { currentUser } = useAuth()
   const [createBillingPortalSession] = useMutation(
     CREATE_BILLING_PORTAL_SESSION,
     {
-      onCompleted: ({ createBillingPortalSession }) => {
+      onCompleted: async ({ createBillingPortalSession }) => {
         const { url } = createBillingPortalSession
-        window.location.assign(url)
+        // window.location.assign(url)
+        // open in new tab
+        toast.success('Opening in a new window tab')
+        await timeout(2000) //for 2 seconds delay
+        const newWindow = window.open(url, '_blank')
+        if (
+          !newWindow ||
+          newWindow.closed ||
+          typeof newWindow.closed === 'undefined'
+        ) {
+          alert(
+            'A popup blocker may have prevented the Stripe Checkout from opening. Please disable the popup blocker and try again.'
+          )
+        }
       },
       onError: (error) => {
-        console.error('Error creating Stripe billing portal session:', error)
+        toast.error('Error creating Stripe billing portal session:', error)
       },
     }
   )
@@ -24,7 +42,14 @@ const StripePortal = () => {
     createBillingPortalSession({ variables: { customerId, returnUrl } })
   }
 
-  return <button onClick={redirectToBillingPortal}>Manage Subscription</button>
+  return (
+    <button
+      className="opacity-70 btn-primary rounded-lg bg-gray-600 py-3 px-2 font-semibold hover:bg-gray-400"
+      onClick={redirectToBillingPortal}
+    >
+      Manage Subscription
+    </button>
+  )
 }
 
 export default StripePortal
