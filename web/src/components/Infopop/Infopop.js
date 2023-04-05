d13a206994cc1588abefd4aa9aa0a09973fd789c
rozbeh <rouzbeh.asghari@gmail.com>
2023-04-04 21:59:19 -0700

    fix infoPop button

--- web/src/components/Infopop/Infopop.js
+++ web/src/components/Infopop/Infopop.js
@@ -10,16 +10,15 @@ import {
   Popover,
   PopoverHandler,
   PopoverContent,
-  Button,
 } from '@material-tailwind/react'
 
 export const Infopop = (inputText) => {
   return (
     <Popover placement="top">
       <PopoverHandler>
-        <Button className="normal-case bg-gray-300 text-black text-sm rounded-50 h-4 w-4">
+        <button className="normal-case bg-gray-300 text-black text-sm rounded-50 h-4 w-4">
           ?
-        </Button>
+        </button>
       </PopoverHandler>
       <PopoverContent toggle="popover" className="arrow border-2 rounded-md">
         <div className="font-bold border-b-2">{inputText.title}</div>
